24c281b8e771dca9e583f85e3682f85bcaf9a90a
Aman Jain <amanjain@cisco.com>
2021-02-22 13:38:32 +0530

    updated redirect uri

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   client_id: 'C363c710f07a19092b369a1006c6a022646e76bc3a04f321e3d9924281813292a',
   client_secret: 'd6282b3b9cc349cfa0b7a42b212c12324e17ad2146a656cbcad10fe69bf6c657',
-  redirect_uri: 'https://amanjain201.github.io/cisco-codefestapp',
+  redirect_uri: 'https://amanjain201.github.io/wxt-warriors/webex',
   scope: 'spark:all spark:kms'
 };
